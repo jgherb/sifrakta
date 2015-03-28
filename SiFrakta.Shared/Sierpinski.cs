25bdef2d66a4ba1ba326f9429b024b8287077cd6
jgherb <jgherb@live.de>
2015-03-28 13:02:44 +0100

    Update Sierpinski.cs

--- SiFrakta.Shared/Sierpinski.cs
+++ SiFrakta.Shared/Sierpinski.cs
@@ -1,3 +1,4 @@
+using SiFrakta_D;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +11,12 @@ namespace SiFrakta
         {
 
         }
+        static int ecken = 3;
+        static int width = 1000;
+        static int height = 600;
+        static int[,] Punkt = new int[ecken, 2];
+        static int[,] Punkt3 = Polygon.GetKoordinaten(3, width, height);
+        static int[,] Punkt5 = Polygon.GetKoordinaten(5, width, height);
         public Sierpinski(int h, int w, int t)
         {
             height = h;
@@ -20,20 +27,11 @@ namespace SiFrakta
                 width = (int)Math.Sqrt((height * height));
             }
         }
-        int height = 0;
-        int width = 0;
         int vtiefe = 0;
 
         internal byte[] Draw(int width, int height, int vt, int fd)
         {
             vtiefe = vt;
-            int[,] Punkt3 = new int[3, 2];
-            Punkt3[0, 0] = 0;
-            Punkt3[0, 1] = height / 2;
-            Punkt3[1, 0] = (int)Math.Sqrt((height * height) * 3 / 4);
-            Punkt3[1, 1] = 0;
-            Punkt3[2, 0] = (int)Math.Sqrt((height * height) * 3 / 4);
-            Punkt3[2, 1] = height;
             // 4 bytes (RGBA) required for each pixel
             byte[] result = new byte[width * height * 4];
             int resultIndex = 0;
@@ -55,9 +53,9 @@ namespace SiFrakta
                 dat[0] = (dat[0] + Punkt3[pkt, 0]) / 2;
                 dat[1] = (dat[1] + Punkt3[pkt, 1]) / 2;
                 // Shade pixel based on probability it's in the set
-                if (result[(dat[0] * width + dat[1]) * 4 + 3] + fd < 256)
+                if (result[(dat[1] * width + dat[0]) * 4 + 3] + fd < 256)
                 {
-                    result[(dat[0] * width + dat[1]) * 4 + 1] = (byte)(result[(dat[0] * width + dat[1]) * 4 + 1] + fd);// Green value of pixel
+                    result[(dat[1] * width + dat[0]) * 4 + 1] = (byte)(result[(dat[1] * width + dat[0]) * 4 + 1] + fd);// Green value of pixel
                 }
             }
 
@@ -66,44 +64,54 @@ namespace SiFrakta
         }
         internal byte[] Draw5(int width, int height, int vt, int fd)
         {
-            double a = (height / (Math.Sin(72 * Math.PI / 180) + Math.Sin(36 * Math.PI / 180)));
             vtiefe = vt*50;
-            int[,] Punkt3 = new int[5, 2];
-            Punkt3[0, 0] = 0;
-            Punkt3[0, 1] = (int)(Math.Cos(72 * Math.PI / 180) * a);
-            Punkt3[1, 0] = 0;
-            Punkt3[1, 1] = (int)(Math.Cos(72 * Math.PI / 180) * a + a);
-            Punkt3[2, 0] = (int)(Math.Sin(72 * Math.PI / 180) * a);
-            Punkt3[2, 1] = 0;
-            Punkt3[3, 0] = (int)(Math.Sin(72 * Math.PI / 180) * a);
-            Punkt3[3, 1] = (int)(2 * Math.Cos(72 * Math.PI / 180) * a + a);
-            Punkt3[4, 0] = (int)(Math.Sin(36 * Math.PI / 180) * a + Math.Sin(72 * Math.PI / 180) * a);
-            Punkt3[4, 1] = (int)(2 * Math.Cos(72 * Math.PI / 180) * a + a) / 2;
-            // 4 bytes (RGBA) required for each pixel
             byte[] result = new byte[width * height * 4];
-            int resultIndex = 0;
-
-            // Max iterations when testing whether a point is in the set
-            int maxIterationCount = 50;
-
-            // Initialisierungen / Deklarationen
             Random zufall = new Random();
-
-            // Plot the Mandelbrot set on x-y plane
-            //Zufallszahl für verwendeten Eckpunkt wird generiert
             int[] dat = new int[2];
-            dat[1] = (int)(Math.Cos(72 * Math.PI / 180) * a);
+            dat[1] = Punkt5[0, 1];
+            dat[0] = Punkt5[0, 0];
             for (int i = 0; i < vtiefe - 1; i++)
             {
                 int pkt = zufall.Next(5);
+                dat[0] = (dat[0] + Punkt5[pkt, 0]) / 2;
+                dat[1] = (dat[1] + Punkt5[pkt, 1]) / 2;
+                if (result[(dat[1] * width + dat[0]) * 4 + 3] + fd < 256)
+                {
+                    result[(dat[1] * width + dat[0]) * 4 + 1] = (byte)(result[(dat[1] * width + dat[0]) * 4 + 1] + fd);
+                }
+            }
+            return result;
+        }
+        internal byte[] DrawC(int ecken_, int width_, int height_, int vt, int fd)
+        {
+            if ((ecken_ != ecken)|(width_!=width)|(height_!=height))
+            {
+                Punkt = Polygon.GetKoordinaten(ecken_, width_, height_);
+                ecken = ecken_;
+                width = width_;
+                height = height_;
+            }
+            double a = (height / (Math.Sin(72 * Math.PI / 180) + Math.Sin(36 * Math.PI / 180)));
+            vtiefe = vt * 50;
+            // 4 bytes (RGBA) required for each pixel
+            byte[] result = new byte[width * height * 4];
+            // Initialisierungen / Deklarationen
+            Random zufall = new Random();
+            //Zufallszahl für verwendeten Eckpunkt wird generiert
+            int[] dat = new int[2];
+            dat[0] = Punkt[0, 0];
+            dat[1] = Punkt[0, 1];
+            for (int i = 0; i < vtiefe - 1; i++)
+            {
+                int pkt = zufall.Next(ecken);
                 //Koordinaten werden bestimmt (Durchschnitt aus altem Punkt und Eckpunkt)
-                dat[0] = (dat[0] + Punkt3[pkt, 0]) / 2;
-                dat[1] = (dat[1] + Punkt3[pkt, 1]) / 2;
+                dat[0] = (dat[0] + Punkt[pkt, 0]) / 2;
+                dat[1] = (dat[1] + Punkt[pkt, 1]) / 2;
 
                 // Shade pixel based on probability it's in the set
-                if (result[(dat[0] * width + dat[1]) * 4 + 3] + fd < 256)
+                if (result[(dat[1] * width + dat[0]) * 4 + 3] + fd < 256)
                 {
-                    result[(dat[0] * width + dat[1]) * 4 + 1] = (byte)(result[(dat[0] * width + dat[1]) * 4 + 1] + fd);// Green value of pixel
+                    result[(dat[1] * width + dat[0]) * 4 + 1] = (byte)(result[(dat[1] * width + dat[0]) * 4 + 1] + fd);// Green value of pixel
                 }
             }
 
